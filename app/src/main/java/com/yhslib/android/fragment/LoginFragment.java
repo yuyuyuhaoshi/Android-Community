19c418965c8c2ea4e82bfffffe3f1fdc8e4eaf59
luoshan <33414408+luosh3n@users.noreply.github.com>
2018-05-28 23:53:08 +0800

    添加登录及注册页面

---
+++ app/src/main/java/com/yhslib/android/fragment/LoginFragment.java
@@ -0,0 +1,38 @@
+package com.yhslib.android.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yhslib.android.R;
+
+public class LoginFragment extends Fragment {
+    private String TAG = "LoginFragment";
+
+    private View view;
+
+    public static LoginFragment newInstance() {
+        Bundle args = new Bundle();
+        LoginFragment fragment = new LoginFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_login, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, TAG);
+    }
+}
---
+++ app/src/main/java/com/yhslib/android/fragment/RegisterFragment.java
@@ -0,0 +1,37 @@
+package com.yhslib.android.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.yhslib.android.R;
+
+public class RegisterFragment extends Fragment {
+    private String TAG = "RegisterFragment";
+
+    private View view;
+
+    public static RegisterFragment newInstance() {
+        Bundle args = new Bundle();
+        RegisterFragment fragment = new RegisterFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.activity_register, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d(TAG, TAG);
+    }
+}
